1b4cbf4b4636b2c68cce5d2e8fb07ed73958726d
Vidya Prakash Awasthi <prakashvidya68@gmail.com>
2025-11-03 18:03:43 +0530

    initial commit

---
+++ test/unified_ai_sdk_test.dart
@@ -0,0 +1,16 @@
+import 'package:unified_ai_sdk/unified_ai_sdk.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
